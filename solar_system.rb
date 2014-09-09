44c3fc3f2e773035b694e56cb9df68d626900e02
Allie Sterling <sterlina89@gmail.com>
2014-09-09 16:06:47 -0700

    fixed local planet date

--- solar_system.rb
+++ solar_system.rb
@@ -10,13 +10,6 @@ class Planet
     @rate_of_solar_rotation = planet_hash[:rate_of_solar_rotation]
   end
 
-  #def find_rate_of_rotation(hours, minutes, seconds)
-  #  seconds = seconds.to_i
-  #  mins_in_sec = minutes.to_i * 60
-  #  hours_in_sec = hours.to_i * 60 * 60
-  #  @rate_of_solar_rotation = seconds + mins_in_sec + hours_in_sec
-  #end
-
 end
 
 class SolarSystem
@@ -24,7 +17,9 @@ class SolarSystem
 
   def initialize
     @planets = []
-    @formation_date = 0
+    @formation_date = -4.5e9 + 2014
+    @current_year = 2014
+    our_solar_system
   end
 
   def add_planet(name)
@@ -33,11 +28,8 @@ class SolarSystem
   end
 
   def find_local_year(planet)
-    #form_date_in_days = @formation_date
-    #local_year_in_days = (2014 - (form_date_in_days) / planet.rate_of_solar_rotation)
-    #local_year = local_year_in_days /
-    local_year = (100 - @formation_date) / planet.rate_of_solar_rotation
-    return local_year
+    local_year = ((@current_year - @formation_date) / planet.rate_of_solar_rotation)
+    return local_year.to_i
   end
 
   def show_planets
@@ -55,12 +47,18 @@ class SolarSystem
   def start
     puts "Which planet would you like to explore? Choose one of the planets below."
     puts
+    puts "To exit, type \"exit\""
+    puts
     @planets.each do |p|
       puts p.name
     end
     puts
     print "> "
     input = gets.chomp
+    if input == "exit"
+      puts "Goodbye!"
+      exit
+    end
     @planets.each do |p|
         if p.name.include? input
           @current_planet = p
@@ -68,10 +66,7 @@ class SolarSystem
     end
     puts
     explore_planet(@current_planet)
-    #puts "Mass: #{@current_planet.mass_in_kg} kg"
-    #puts "Distance from sun: #{@current_planet.dist_sun} miles"
-    #puts "This planet has #{@current_planet.num_of_moons} moons."
-    #puts
+
   end
 
   def explore_planet(planet)
@@ -92,7 +87,7 @@ To exit completely, type \"exit\".
     input = gets.chomp
     while input
       if input == "1"
-        puts "\n#{planet.name}'s mass is #{planet.mass_in_kg}."
+        puts "\n#{planet.name}'s mass is #{planet.mass_in_kg} kg."
         puts
         puts prompt
         input = gets.chomp
@@ -102,7 +97,7 @@ To exit completely, type \"exit\".
         puts prompt
         input = gets.chomp
       elsif input == "3"
-        puts "\n#{planet.name} has #{planet.num_moons} moons."
+        puts "\n#{planet.name} has #{planet.num_of_moons} moons."
         puts
         puts prompt
         input = gets.chomp
@@ -112,7 +107,7 @@ To exit completely, type \"exit\".
         puts prompt
         input = gets.chomp
       elsif input == "5"
-        puts "\nThe local year on #{planet.name} is #{planet.find_local_year}."
+        puts "\nThe local year on #{planet.name} is #{find_local_year(planet)}."
         puts
         puts prompt
         input = gets.chomp
@@ -120,6 +115,7 @@ To exit completely, type \"exit\".
         puts "Goodbye!"
         exit
       elsif input == "planet menu"
+        puts
         start
       else
         puts "Sorry, I didn't understand that.  Please type 1, 2, 3, 4, or 5 to choose an option."
@@ -130,24 +126,17 @@ To exit completely, type \"exit\".
   end
 
   def our_solar_system
-    add_planet(name: "Mercury", mass_in_kg: 328.5e21, dist_sun: 35_980_000, num_of_moons: 0, rate_of_solar_rotation: 10)
-    add_planet(name: "Venus", mass_in_kg: 4.867e24, dist_sun: 67_240_000, num_of_moons: 0)
+    add_planet(name: "Mercury", mass_in_kg: 328.5e21, dist_sun: 35_980_000, num_of_moons: 0, rate_of_solar_rotation: 0.24)
+    add_planet(name: "Venus", mass_in_kg: 4.867e24, dist_sun: 67_240_000, num_of_moons: 0, rate_of_solar_rotation: 0.62)
     add_planet(name: "Earth", mass_in_kg: 5.972e24, dist_sun: 92_960_000, num_of_moons: 1, rate_of_solar_rotation: 1)
-    add_planet(name: "Mars", mass_in_kg: 639e21, dist_sun: 141_600_000, num_of_moons: 2)
-    add_planet(name: "Jupiter", mass_in_kg: 1.898e27, dist_sun: 483_800_000, num_of_moons: 50)
-    add_planet(name: "Saturn", mass_in_kg: 568.3e24, dist_sun: 890_700_000, num_of_moons: 53)
-    add_planet(name: "Uranus", mass_in_kg: 86.81e24, dist_sun: 1_787_000_000, num_of_moons: 27)
-    add_planet(name: "Neptune", mass_in_kg: 102.4e24, dist_sun: 2_798_000_000, num_of_moons: 13)
+    add_planet(name: "Mars", mass_in_kg: 639e21, dist_sun: 141_600_000, num_of_moons: 2, rate_of_solar_rotation: 1.88)
+    add_planet(name: "Jupiter", mass_in_kg: 1.898e27, dist_sun: 483_800_000, num_of_moons: 50, rate_of_solar_rotation: 11.86)
+    add_planet(name: "Saturn", mass_in_kg: 568.3e24, dist_sun: 890_700_000, num_of_moons: 53, rate_of_solar_rotation: 29.45)
+    add_planet(name: "Uranus", mass_in_kg: 86.81e24, dist_sun: 1_787_000_000, num_of_moons: 27, rate_of_solar_rotation: 84.3)
+    add_planet(name: "Neptune", mass_in_kg: 102.4e24, dist_sun: 2_798_000_000, num_of_moons: 13, rate_of_solar_rotation: 164.79)
   end
 
 end
 
 s = SolarSystem.new
-s.our_solar_system
-
-puts
-s.show_planets
-puts "Earth year is: #{s.find_local_year(s.planets[2])} "
-puts "Local year is: #{s.find_local_year(s.planets[0])} "
-
 s.start
